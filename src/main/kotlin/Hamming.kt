6587264ab0acc94092dba30691ab962684cb5b6f
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-03 15:54:00 +0300

    Remove unnecessary check for null

--- src/main/kotlin/Hamming.kt
+++ src/main/kotlin/Hamming.kt
@@ -1,9 +1,6 @@
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        if (leftStrand.isNullOrBlank() && rightStrand.isNullOrBlank()) {
-            return 0
-        }
         require(leftStrand.length == rightStrand.length) { "left and right strands must be of equal length" }
         return leftStrand.zip(rightStrand).count { pair ->
             pair.first != pair.second
