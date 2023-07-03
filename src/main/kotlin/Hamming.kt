e19d32408c98947c180873e1531bfe29a69d4d12
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-03 15:48:47 +0300

    Use 'require' instead of 'throw IllegalArgumentException'

--- src/main/kotlin/Hamming.kt
+++ src/main/kotlin/Hamming.kt
@@ -4,9 +4,7 @@ object Hamming {
         if (leftStrand.isNullOrBlank() && rightStrand.isNullOrBlank()) {
             return 0
         }
-        if (leftStrand.length != rightStrand.length) {
-            throw IllegalArgumentException("left and right strands must be of equal length")
-        }
+        require(leftStrand.length == rightStrand.length) { "left and right strands must be of equal length" }
         return leftStrand.foldIndexed(
             0
         ) { index, acc, c -> if (rightStrand[index] != c) acc + 1 else acc }
