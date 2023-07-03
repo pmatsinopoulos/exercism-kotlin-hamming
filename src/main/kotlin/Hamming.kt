7a5d7289dad6469b7576916fc59b1c4c0afe773b
Panos Matsinopoulos <panagiotis@matsinopoulos.gr>
2023-07-03 15:24:45 +0300

    First iteration with simple straight forward solution

--- src/main/kotlin/Hamming.kt
+++ src/main/kotlin/Hamming.kt
@@ -1,6 +1,18 @@
 object Hamming {
 
     fun compute(leftStrand: String, rightStrand: String): Int {
-        TODO("Implement this function to complete the task")
+        if (leftStrand.isNullOrBlank() && rightStrand.isNullOrBlank()) {
+            return 0
+        }
+        if (leftStrand.length != rightStrand.length) {
+            throw IllegalArgumentException("left and right strands must be of equal length")
+        }
+        var result = 0
+        leftStrand.forEachIndexed { index, c ->
+            if (rightStrand[index] != c) {
+                result += 1
+            }
+        }
+        return result
     }
 }
